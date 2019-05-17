1313e81b0e501f794c326f0d0594c47fbb0e5420
Sebastian Vermaas <flowpineapple@gmail.com>
2019-05-17 11:03:16 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='network_visualization',
+      version='0.1',
+      description='Visualization package for drawing Keras network graphs',
+      url='https://github.com/pineappleflow/network-visualization-keras',
+      author='Sebastian Vermaas',
+      author_email='flowpineapple@gmail.com',
+      packages=find_packages())
